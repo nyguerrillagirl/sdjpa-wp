1e13712170cf914c0365ec7ddc16b139569c4524
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-12-07 12:37:34 -0500

    Completed Lecture 179 - Basic Annotation.

--- src/main/java/guru/springframework/sdjpa/wp/domain/User.java
+++ src/main/java/guru/springframework/sdjpa/wp/domain/User.java
@@ -36,7 +36,7 @@ public class User {
     @Column(name = "user_status")
     private Integer status;
 
-    @Column(name="display_name")
+    @Basic(optional = false)
     private String displayName;
 
     public Long getId() {
