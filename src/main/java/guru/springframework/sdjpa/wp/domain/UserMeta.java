7c6a5596389198ad69c6f930a10c9701e708cc11
nyguerrillagirl <lorraine.figueroa@brainycode.com>
2023-12-07 12:27:19 -0500

    Completed Lecture 178 - LOB Columns.

---
+++ src/main/java/guru/springframework/sdjpa/wp/domain/UserMeta.java
@@ -0,0 +1,51 @@
+package guru.springframework.sdjpa.wp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "wp_usermeta")
+public class UserMeta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "umeta_id")
+    private Long id;
+
+    private Long userId;
+
+    private String metaKey;
+
+    @Lob
+    private String metaValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMetaKey() {
+        return metaKey;
+    }
+
+    public void setMetaKey(String metaKey) {
+        this.metaKey = metaKey;
+    }
+
+    public String getMetaValue() {
+        return metaValue;
+    }
+
+    public void setMetaValue(String metaValue) {
+        this.metaValue = metaValue;
+    }
+}
